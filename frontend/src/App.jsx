301d4454a566c9198b43907dd5a8aeb71a4944ad
Aneke Ifeany <anekeiifeanyi@gmail.com>
2025-10-31 19:36:46 +0100

    completed the landing page, login page, register page, and the user dashboard page for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,14 +3,22 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import UserLayout from "./components/Layout/UserLayout";
 import Home from "./pages/Home";
 import { Toaster } from "sonner";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
 
 const App = () => {
   return (
-    <BrowserRouter>
+    <BrowserRouter
+      future={{ v7_startTransiton: true, v7_elativeSplatPath: true }}
+    >
       <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<UserLayout />}>
           <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="profile" element={<Profile />} />
         </Route>
         <Route>{/* {Admin layout} */}</Route>
       </Routes>
